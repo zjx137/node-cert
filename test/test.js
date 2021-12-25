c4091d2b177a8c7eed327508eca0d5df3f74f2c9
bacbin <905980546@qq.com>
2021-12-25 16:08:51 +0800

    release 1.0.0

--- test/test.js
+++ test/test.js
@@ -5,11 +5,10 @@ const path = require('path')
 
 async function test() {
     let option = {}
-    let a = new Cert(option)
-    let res = await a.getRootCAKey()
-    let res2 = await a.getHostCACert('baidu.com')
-    console.log(res2)
-    
+    let certMgr = new Cert(option)
+    let root = await certMgr.getRootCAKey()
+    let res = await certMgr.getHostCACert('baidu.com')
+    console.log(res)
 }
 
 test()
