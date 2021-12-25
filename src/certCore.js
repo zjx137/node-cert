ecea0c4c3c1e2dab4cfb1636058a511d33b80acd
bacbin <905980546@qq.com>
2021-12-25 12:48:44 +0800

    add generateCertByHost

--- src/certCore.js
+++ src/certCore.js
@@ -15,14 +15,14 @@ class certCore {
         if(!fs.existsSync(this.rootCAPath)) {
             fs.mkdirSync(this.rootCAPath)
         }
+        // Generate root KEY/CRT Path
         this.rootCAKeyPath = path.join(this.rootCAPath, 'rootCA.key')
         this.rootCACertPath = path.join(this.rootCAPath, 'rootCA.crt')
     }
-    getA() {
-        return this.keyPair
-    }
     getKeysAndCert (serialNumber) {
+        // Generate RSA Keys Pair
         const keys = forge.pki.rsa.generateKeyPair(this.keyPair)
+        // Generate cert
         const cert = forge.pki.createCertificate()
         cert.publicKey = keys.publicKey // 公钥塞进证书
         cert.serialNumber = serialNumber || (Math.floor(Math.random() * 100000) + '')
@@ -36,8 +36,57 @@ class certCore {
             cert
         }
     }
-    generateCertByHost (host) {
+    generateCertByHost (host, rootCA) {
+        const hostCAKeyPath = path.join(this.rootCAPath, `${host}.key`)
+        const hostCACertPath = path.join(this.rootCAPath, `${host}.crt`)
+        return new Promise((resolve, reject) => {
+            // Use Root Crt sign Host Crt
+            if (this.isHostCAFilesExist(hostCACertPath, hostCAKeyPath)) {
+                console.log('existed1')
+                const cert = fs.readFileSync(hostCACertPath).toString()
+                const privateKey = fs.readFileSync(hostCAKeyPath).toString()
+                const publicKey = forge.pki.publicKeyToPem(forge.pki.certificateFromPem(cert).publicKey)
+                resolve({
+                    cert,
+                    privateKey,
+                    publicKey
+                }) 
+            } else {
+                const md = forge.md.md5.create()
+                md.update(host)
+                const { keys, cert } = this.getKeysAndCert(md.digest().toHex())
 
+                const caKey = forge.pki.privateKeyFromPem(rootCA.privateKey)
+                const caCert = forge.pki.certificateFromPem(rootCA.cert)
+
+                cert.setIssuer(caCert.subject.attributes)
+                const attrs = this.defaultAttrs.concat([
+                    {
+                        name: 'commonName',
+                        value: host
+                    }
+                ])
+                const extensions = [
+                    { name: 'basicConstraints', cA: true },
+                    this.getExtensionSAN(host)
+                ]
+                cert.setSubject(attrs)
+                cert.setExtensions(extensions)
+                cert.sign(caKey, forge.md.sha256.create())
+                
+                fs.writeFileSync(hostCACertPath, forge.pki.certificateToPem(cert))
+                fs.writeFileSync(hostCAKeyPath, forge.pki.privateKeyToPem(keys.privateKey))
+
+                resolve ({
+                    privateKey: forge.pki.privateKeyToPem(keys.privateKey),
+                    publicKey: forge.pki.publicKeyToPem(keys.publicKey),
+                    cert: forge.pki.certificateToPem(cert)
+                })
+            }
+            
+        })
+        
+        
     }
     generateRootCert (commonName = 'certCore') {
         return new Promise((resolve, reject) => {
@@ -87,7 +136,10 @@ class certCore {
         })
     }
     isRootCAFilesExist () {
-        return (fs.existsSync(this.rootCAKeyPath)&&fs.existsSync(this.rootCACertPath))
+        return (fs.existsSync(this.rootCAKeyPath) && fs.existsSync(this.rootCACertPath))
+    }
+    isHostCAFilesExist (certPath, keyPath) {
+        return (fs.existsSync(certPath) && fs.existsSync(keyPath))
     }
     clertCert () {
         const rootCAPath = this.rootCAPath
@@ -96,6 +148,20 @@ class certCore {
                 const curPath = path.join(rootCAPath, file)
                 fs.unlinkSync(curPath)
             })
+        }
+    }
+    getExtensionSAN (domain = '') {
+        const isIp = /^\d+?\.\d+?\.\d+?\.\d+?$/.test(domain)
+        if (isIp) {
+            return {
+                name: 'subjectAltName',
+                altNames: [{ type: 7, ip: domain}]
+            }
+        } else {
+            return {
+                name: 'subjectAltName',
+                altNames: [{ type: 2, value: domain}]
+            }
         }
     }
 }
